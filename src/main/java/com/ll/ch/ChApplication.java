0de332e8917972e2266ebfbab5cacda32732ddfb
ysne0720 <parkyuns1316@naver.com>
2024-11-23 12:22:25 +0900

    ㄴãchating

---
+++ src/main/java/com/ll/ch/ChApplication.java
@@ -0,0 +1,13 @@
+package com.ll.ch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/ch/ChApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.ch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
